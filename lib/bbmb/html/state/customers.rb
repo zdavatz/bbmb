4186b710a758af12d2a687e17a3270fb97fb9fa7
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2016-07-18 16:57:39 +0200

    Add missing sorting for valid and last_login
    * Create Decorator class
    * These columns need a query to yus session

--- lib/bbmb/html/state/customers.rb
+++ lib/bbmb/html/state/customers.rb
@@ -1,10 +1,10 @@
 # Html::State::Customers -- bbmb.ch -- 18.09.2006 -- hwyss@ywesee.com
 
+require 'ostruct'
 require 'bbmb/html/state/global_predefine'
 require 'bbmb/html/state/customer'
 require 'bbmb/html/view/customers'
 require 'bbmb/model/customer'
-require 'ostruct'
 
 module BBMB
   module Html
@@ -15,13 +15,29 @@ class Customers < Global
   FILTER = [ :customer_id, :organisation, :plz, :city, :email ]
   def init
     start = Time.now
-    @model = BBMB.persistence.all(Model::Customer)
+    @model  = BBMB.persistence.all(Model::Customer)
     @sortby = [:organisation]
     BBMB.logger.debug('State') {
       sprintf("Customers#init: loaded %i customers in %1.5fs",
               @model.size, Time.now - start)
     }
-    @filter = Proc.new { |model|
+    @filter = make_filter
+  end
+  def page(model)
+    page = OpenStruct.new
+    index = @session.user_input(:index).to_i
+    step = @session.user.pagestep || BBMB.config.pagestep
+    page.index = index
+    page.first = index + 1
+    page.step = step
+    page.total = model.size
+    page.customers = model[index, step]
+    page.last = index + page.customers.size
+    page
+  end
+  private
+  def make_filter
+    Proc.new { |model|
       input = @session.event_bound_user_input(:filter)
       if input
         pattern = Regexp.new(input.gsub('*', '.*'), 'ui')
@@ -38,22 +54,27 @@ class Customers < Global
         }
       end
       get_sortby!
+      if @sortby.include?(:valid) || @sortby.include?(:last_login)
+        # decorates customer objects with (yus) session
+        model = model.map { |m| CustomerDecorator.new(m, @session) }
+      end
       model.sort! { |a, b| compare_entries(a, b) }
-      @sort_reverse && model.reverse! 
+      @sort_reverse && model.reverse!
       page(model)
     }
   end
-  def page(model)
-    page = OpenStruct.new
-    index = @session.user_input(:index).to_i
-    step = @session.user.pagestep || BBMB.config.pagestep
-    page.index = index
-    page.first = index + 1
-    page.step = step
-    page.total = model.size
-    page.customers = model[index, step]
-    page.last = index + page.customers.size
-    page
+end
+class CustomerDecorator < SimpleDelegator
+  def initialize(model, session)
+    @source  = model
+    @session = session
+    self.__setobj__(@source)
+  end
+  def last_login
+    @last_login ||= @session.user.last_login(email)
+  end
+  def valid
+    @valid ||= @session.user.entity_valid?(email).to_s
   end
 end
     end
